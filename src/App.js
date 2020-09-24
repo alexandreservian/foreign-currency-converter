a74338811cb21b76afb24b72b56ef22fb94fced8
alexandreservian <alexandreservian@gmail.com>
2020-09-24 20:08:08 -0300

    refactor(app): changed name project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import CurrencyConverter from "components/currency-converter";
 
 function App() {
-	return <div className="App"></div>;
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <CurrencyConverter />
+    </ThemeProvider>
+  );
 }
 
 export default App;
