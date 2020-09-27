4285a9fa9f763281f29b066a2dedb247cc130ec8
alexandreservian <alexandreservian@gmail.com>
2020-09-27 16:32:52 -0300

    refactor(decimal input): added useField

--- src/components/decimal-input/index.js
+++ src/components/decimal-input/index.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { useField } from "formik";
 import NumberFormat from "react-number-format";
 import Container from "./style";
 
-const DecimalInput = () => {
+const DecimalInput = ({ name }) => {
+  const [, , helpers] = useField(name);
+  const { setValue } = helpers;
+
   return (
     <Container>
       <NumberFormat
+        type="tel"
         className="input"
+        name={name}
         thousandSeparator={"."}
         decimalSeparator={","}
         decimalScale={2}
+        onValueChange={({ floatValue }) => setValue(floatValue)}
+        onFocus={event => {
+          event.target.select();
+        }}
       />
     </Container>
   );
