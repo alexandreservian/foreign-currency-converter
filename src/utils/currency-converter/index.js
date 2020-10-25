2e05385597680fd6f76234d301d01f28edc51d9d
alexandreservian <alexandreservian@gmail.com>
2020-10-24 21:10:49 -0300

    refactor(currencyConverter): added tests

--- src/utils/currency-converter/index.js
+++ src/utils/currency-converter/index.js
@@ -1,3 +1,20 @@
-const currencyConverter = () => 12;
+import isNumber from "utils/is-number";
+import generateTypeError from "utils/generate-type-error";
+
+/**
+ * Converts a foreign currency into another
+ * @function
+ * @param  {number} [money=0] Amount of Money
+ * @param  {number} [rate=1] Value of a foreign currency
+ * @return {number} Return converted amount
+ */
+
+const currencyConverter = (money = 0, rate = 1) => {
+  return !isNumber(money)
+    ? generateTypeError("The first parameter must be an number type")
+    : !isNumber(rate)
+    ? generateTypeError("The second parameter must be an number type")
+    : money * rate;
+};
 
 export default currencyConverter;
