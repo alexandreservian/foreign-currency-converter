dbd5ff996c13fd67e8f316ad75ea34c750c18ea9
alexandreservian <alexandreservian@gmail.com>
2020-11-08 17:57:52 -0300

    test(form currency converter): added new test to test button 'ReverseCurrency'

--- src/components/form-currency-converter/index.spec.js
+++ src/components/form-currency-converter/index.spec.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import FormCurrencyConverter from ".";
 
 describe("Form Currency Converter", () => {
@@ -7,6 +7,27 @@ describe("Form Currency Converter", () => {
     const { container } = createComponent();
 
     expect(container.firstChild).toBeInTheDocument();
+  });
+
+  it("should change values of select 'From Currenty' to 'To Currenty' when clicked in button 'ReverseCurrency'", async () => {
+    createComponent();
+    const buttonReverse = screen.getByRole("button", {
+      name: "Flip Currencies",
+    });
+    const selectFromCurrenty = screen.getByRole("combobox", {
+      name: "From Currenty",
+    });
+    const selectToCurrenty = screen.getByRole("combobox", {
+      name: "To Currenty",
+    });
+
+    const currentValueFromCurrenty = selectFromCurrenty.value;
+    const currentValueToCurrenty = selectToCurrenty.value;
+
+    fireEvent.click(buttonReverse);
+
+    expect(selectFromCurrenty.value).toEqual(currentValueToCurrenty);
+    expect(selectToCurrenty.value).toEqual(currentValueFromCurrenty);
   });
 });
 
