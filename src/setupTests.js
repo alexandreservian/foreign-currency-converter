716296a239c7ce1d07bd3d0953b0aa3b6567cadd
alexandreservian <alexandreservian@gmail.com>
2020-11-08 17:54:36 -0300

    test: added mock to react-select

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,4 +2,31 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
-import '@testing-library/jest-dom/extend-expect';
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+
+jest.mock(
+  "react-select",
+  () => ({ "aria-label": ariaLabel, options, value, onChange, ...props }) => {
+    const { value: currentValueSelect } = value;
+    function handleChange(event) {
+      const option = options.find(
+        option => option.value === event.currentTarget.value
+      );
+      onChange(option);
+    }
+    return (
+      <select
+        aria-label={ariaLabel}
+        value={currentValueSelect}
+        onChange={handleChange}
+      >
+        {options.map(({ label, value }) => (
+          <option key={value} value={value}>
+            {label}
+          </option>
+        ))}
+      </select>
+    );
+  }
+);
